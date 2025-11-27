1a447f34adcc6bee702ab952cdb7b2082e8b00e6
AndriwRC <randriw4@gmail.com>
2025-11-26 22:59:40 -0500

    refactor(users): configure a hybrid RBAC using Enum for permissions definition

--- app/features/users/models.py
+++ app/features/users/models.py
@@ -3,6 +3,7 @@ from typing import Optional, TYPE_CHECKING
 from sqlmodel import Field, Relationship, SQLModel
 
 from .schemas import UserBase
+from .permissions import PermissionEnum
 from ..visits.models import UserVisitLink
 
 if TYPE_CHECKING:
@@ -20,37 +21,31 @@ class RolePermissionLink(SQLModel, table=True):
     )
 
 
+class Permission(SQLModel, table=True):
+    __tablename__ = "permissions"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: PermissionEnum = Field(unique=True)
+    label: str
+    module: str
+    description: Optional[str] = None
+
+    roles: list["Role"] = Relationship(
+        back_populates="permissions", link_model=RolePermissionLink
+    )
+
+
 class Role(SQLModel, table=True):
     __tablename__ = "roles"
 
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
+    is_protected: bool = Field(default=False)
 
-    permissions: list["Permission"] = Relationship(
+    permissions: list[Permission] = Relationship(
         back_populates="roles", link_model=RolePermissionLink
     )
-
-
-class Permission(SQLModel, table=True):
-    __tablename__ = "permissions"
-
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
-
-    roles: list[Role] = Relationship(
-        back_populates="permissions", link_model=RolePermissionLink
-    )
-
-
-class UserRoleLink(SQLModel, table=True):
-    __tablename__ = "user_role_link"
-
-    user_id: Optional[int] = Field(
-        default=None, foreign_key="users.id", primary_key=True
-    )
-    role_id: Optional[int] = Field(
-        default=None, foreign_key="roles.id", primary_key=True
-    )
+    users: list["User"] = Relationship(back_populates="role")
 
 
 class User(UserBase, table=True):
@@ -58,11 +53,12 @@ class User(UserBase, table=True):
 
     id: Optional[int] = Field(default=None, primary_key=True)
     email: str = Field(unique=True)
+    role_id: Optional[int] = Field(default=None, foreign_key="roles.id")
     created_at: Optional[datetime] = Field(default_factory=datetime.now, nullable=True)
     updated_at: Optional[datetime] = Field(default_factory=datetime.now, nullable=True)
     deleted_at: Optional[datetime] = Field(default=None, nullable=True)
 
-    roles: list[Role] = Relationship(link_model=UserRoleLink)
+    role: Optional[Role] = Relationship(back_populates="users")
     hours: list["Hour"] = Relationship(back_populates="user")
     scheduled_visits: list["Visit"] = Relationship(back_populates="responsible")
     attended_visits: list["Visit"] = Relationship(
