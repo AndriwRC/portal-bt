9e3c09a81c5fdcdc9f6c3a6243e864ca07d6bfe9
AndriwRC <randriw4@gmail.com>
2025-11-29 19:25:22 -0500

    refactor(settings): migrate .env variables handling to pydantic-settings package

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,11 +1,15 @@
-import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings:
-    DEBUG = os.getenv("DEBUG")
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
 
-    DB_NAME = os.getenv("DB_NAME")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = os.getenv("DB_PORT")
+    DEBUG: bool = False
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: str
+    DB_ECHO: bool = False
+
+settings = Settings()
--- app/database/connection.py
+++ app/database/connection.py
@@ -1,7 +1,7 @@
 from sqlmodel import create_engine
-from ..core.settings import Settings
+from ..core.settings import settings
 
 
 class Connection:
-    DB_URI = f"postgresql+psycopg2://{Settings.DB_USER}:{Settings.DB_PASSWORD}@{Settings.DB_HOST}:{Settings.DB_PORT}/{Settings.DB_NAME}"
-    ENGINE = create_engine(url=DB_URI, echo=True)
+    DB_URI = f"postgresql+psycopg2://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+    ENGINE = create_engine(url=DB_URI, echo=settings.DB_ECHO)
