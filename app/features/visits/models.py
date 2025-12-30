b6d9430ab69531d0b95905983b91ee9c2fca0115
Jorge0929 <jorgeandresabino123@gmail.com>
2025-12-30 14:10:48 -0500

    feat(visits): initialize visit model with enum and relationships

--- app/features/visits/models.py
+++ app/features/visits/models.py
@@ -1,11 +1,18 @@
-from datetime import date, time
+from datetime import date, time, datetime
 from typing import TYPE_CHECKING, Optional
 from sqlmodel import Field, Relationship, SQLModel
-
+from enum import Enum
 
 if TYPE_CHECKING:
     from ..users.models import User
     from ..schools.models import School
+
+
+class VisitStatus(str, Enum):
+    PENDING = "PENDING"
+    SCHEDULED = "SCHEDULED"
+    COMPLETED = "COMPLETED"
+    CANCELLED = "CANCELLED"
 
 
 class UserVisitLink(SQLModel, table=True):
@@ -20,14 +27,23 @@ class Visit(SQLModel, table=True):
     __tablename__ = "visits"
 
     id: int | None = Field(default=None, primary_key=True)
-    equipment: str
+
     date: date
     time_start: time
     time_end: time
 
+    status: VisitStatus = Field(default=VisitStatus.PENDING)
+    has_pc: bool = Field(default=False)
+    has_videobeam: bool = Field(default=False)
+    students_number: int | None = Field(default=None)
+    observations: str | None = Field(default=None)
+
+    created_at: datetime | None = Field(default_factory=datetime.now, nullable=True)
+    updated_at: datetime | None = Field(default_factory=datetime.now, nullable=True)
+    deleted_at: datetime | None = Field(default=None, nullable=True)
+
     school_id: int | None = Field(default=None, foreign_key="schools.id")
     responsible_id: int | None = Field(default=None, foreign_key="users.id")
-
     school: Optional["School"] = Relationship(back_populates="visits")
     responsible: Optional["User"] = Relationship(back_populates="scheduled_visits")
     assignees: list["User"] = Relationship(
