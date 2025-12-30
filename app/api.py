67fbf8215f8b12e58deaa95715663e9c27bdc907
Jorge0929 <jorgeandresabino123@gmail.com>
2025-12-29 21:49:39 -0500

    feat(api): register schools router

--- app/api.py
+++ app/api.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from .features.users.routes import router as users_router
 from .features.auth.routes import router as auth_router
+from .features.schools.routes import router as schools_router
+
 
 def register_routes(app: FastAPI):
     app.include_router(auth_router)
     app.include_router(users_router)
+    app.include_router(schools_router)
