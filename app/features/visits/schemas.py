381ed9f37fede2c1c0a1c48bb873ce11f0d572d4
Jorge0929 <jorgeandresabino123@gmail.com>
2026-01-14 14:30:17 -0500

    feat(visits): define schemas for creation, update and detailed reading

--- app/features/visits/schemas.py
+++ app/features/visits/schemas.py
@@ -4,55 +4,52 @@ from sqlmodel import SQLModel
 import datetime
 
 from .models import VisitStatus
-from features.schools.schemas import SchoolRead
-from features.users.schemas import UserRead
+from app.features.schools.schemas import SchoolRead
+from app.features.users.schemas import UserRead
+
 
 class VisitBase(SQLModel):
-    date:datetime.date
-    time_start:datetime.time
-    time_end:datetime.time
+    date: datetime.date
+    time_start: datetime.time
+    time_end: datetime.time
 
 
 class VisitCreate(VisitBase):
-    school_id:int
+    school_id: int
 
-    status:VisitStatus = VisitStatus.SCHEDULED
-    has_pc:bool = False
-    has_videobeam:bool = False
-    students_number:int
-    observations:str | None = None
+    status: VisitStatus = VisitStatus.SCHEDULED
+    has_pc: bool = False
+    has_videobeam: bool = False
+    students_number: int
+    observations: str | None = None
 
 
 class VisitUpdate(SQLModel):
     date: datetime.date | None = None
     time_start: datetime.time | None = None
     time_end: datetime.time | None = None
-    students_number:int | None = None
-    observations:str | None = None
+    students_number: int | None = None
+    observations: str | None = None
 
-    assignee_ids: list[int] | None =  None
+    assignee_ids: list[int] | None = None
 
 
 class VisitRead(VisitBase):
-    id:int
+    id: int
     status: VisitStatus
 
-    school:SchoolRead | None = None
+    school: SchoolRead
 
 
 class VisitReadMin(VisitBase):
     pass
 
+
 class VisitReadDetailed(VisitRead):
-    responsible_name:str
-    has_pc:bool
-    has_videobeam:bool
-    observations:str
-    students_number:int
+    has_pc: bool
+    has_videobeam: bool
+    observations: str | None = None
+    students_number: int
 
     assignees: list[UserRead] = []
-    responsible: UserRead | None = None
-
-
-
-
+    responsible: UserRead
