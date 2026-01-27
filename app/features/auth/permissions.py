43b534a43218a34aad6d5ecd7f78b7142e391f0b
Jorge0929 <jorgeandresabino123@gmail.com>
2026-01-27 17:26:37 -0500

    feat(auth): add school manage permission

--- app/features/auth/permissions.py
+++ app/features/auth/permissions.py
@@ -5,13 +5,14 @@ class PermissionEnum(str, Enum):
     USER_VIEW = "user.view"
     ROLE_MANAGE = "role.manage"
     HOUR_MANAGE = "hour.manage"
+    SCHOOL_MANAGE = "school.manage"
 
 
 PERMISSION_METADATA = {
     PermissionEnum.USER_VIEW: {
         "label": "Ver usuarios",
         "module": "Usuarios",
-        "description": "Listar los usuarios registrados en la plataforma."
+        "description": "Listar los usuarios registrados en la plataforma.",
     },
     PermissionEnum.ROLE_MANAGE: {
         "label": "Administrar roles",
@@ -22,5 +23,10 @@ PERMISSION_METADATA = {
         "label": "Administrar horas",
         "module": "Horas",
         "description": "Aprobar o rechazar horas.",
-    }
+    },
+    PermissionEnum.SCHOOL_MANAGE: {
+        "label": "Eliminar colegios",
+        "module": "Schools",
+        "description": "Eliminar los colegios registrados en la plataforma",
+    },
 }
